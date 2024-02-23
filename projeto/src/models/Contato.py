47e7f3088bee0e4b4a2ed6259557bfa0644b68b4
Kauan <kauansbflu@gmail.com>
2024-02-23 14:36:19 -0300

    atualizando model

--- projeto/src/models/Contato.py
+++ projeto/src/models/Contato.py
@@ -2,6 +2,3 @@ class Contato:
     def __init__(self, nome, telefone):
         self.nome = nome
         self.telefone = telefone
-
-    def __str__(self):
-        return f"{self.nome}: {self.telefone}"
