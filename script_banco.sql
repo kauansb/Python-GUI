3d690dde546e8f0262d72b9756e5b1c6796e9c45
Kauan <kauansbflu@gmail.com>
2024-02-26 20:51:37 -0300

    Create script_banco.sql

---
+++ script_banco.sql
@@ -0,0 +1,24 @@
+drop database if exists projeto_python;
+create database projeto_python;
+use projeto_python;
+
+CREATE TABLE aluno (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    sexo char(1),
+    email VARCHAR(55)
+);
+
+CREATE TABLE cursos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_curso VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE matricula (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    aluno_id INT,
+    curso_id INT,
+    data_matricula DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (aluno_id) REFERENCES aluno(id),
+    FOREIGN KEY (curso_id) REFERENCES cursos(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
