cf26ba3d6cdd911fc4981f05d9463773a0e837c6
Kauan <kauansbflu@gmail.com>
2024-02-28 20:45:36 -0300

    design

--- projeto_ctk/view/App.py
+++ projeto_ctk/view/App.py
@@ -5,7 +5,7 @@ from Crud import Crud
 class Application():
     def __init__(self, app):
         self.app = app
-        app.title("Python GUI - Cadastro de Aluno")
+        app.title("Python GUI")
         app.geometry("650x400")
         self.create_widgets()
         self.crud = Crud()  # Instancie a classe Crud
@@ -13,35 +13,47 @@ class Application():
         self.carregar_lista_alunos()
 
     def create_widgets(self):
-        # Bloco 1: Informações Principais
-        frame = ctk.CTkFrame(self.app, width=200, height=200)
-        frame.grid(row=0, column=0, padx=10, pady=5)
 
-        ctk.CTkLabel(frame, text="Nome:").grid(row=0, column=0, padx=0, pady=2, sticky="e")
-        self.nome_entry = ctk.CTkEntry(frame)
+        # Frame principal
+        main_frame = ctk.CTkFrame(self.app)
+        main_frame.pack(expand=True, fill='both', padx=10, pady=10)
+
+        # Título
+        title_frame = ctk.CTkFrame(main_frame, height=50)
+        title_frame.pack(fill='x', padx=10, pady=(0, 10))
+        ctk.CTkLabel(title_frame, text="Cadastro de Aluno", font=('verdana', 20)).pack(padx=10, pady=5)
+
+        # Frame para informações do aluno
+        info_frame = ctk.CTkFrame(main_frame)
+        info_frame.pack(anchor='n', side='left', padx=10, pady=5)
+
+        ctk.CTkLabel(info_frame, text="Nome:").grid(row=0, column=0, padx=5, pady=2, sticky="e")
+        self.nome_entry = ctk.CTkEntry(info_frame)
         self.nome_entry.grid(row=0, column=1, padx=5, pady=2)
 
+        ctk.CTkLabel(info_frame, text="Gênero:").grid(row=1, column=0, padx=5, pady=2, sticky="e")
         self.radio_var = tk.IntVar(value=0)
-        ctk.CTkRadioButton(frame, text="Masculino", variable=self.radio_var, value=1).grid(row=1, column=0, padx=2, pady=1, sticky="e")
-        ctk.CTkRadioButton(frame, text="Feminino", variable=self.radio_var, value=2).grid(row=1, column=1, padx=2, pady=1, sticky="e")
+        ctk.CTkRadioButton(info_frame, text="Masculino", variable=self.radio_var, value=1).grid(row=1, column=1, padx=2, pady=2, sticky="w")
+        ctk.CTkRadioButton(info_frame, text="Feminino", variable=self.radio_var, value=2).grid(row=2, column=1, padx=2, pady=2, sticky="w")
 
-        ctk.CTkLabel(frame, text="Curso de Tecnologia:").grid(row=2, column=0, padx=5, pady=2, sticky="e")
-        self.curso_combobox = ctk.CTkComboBox(frame, values=["Python", "JavaScript", "Java", "PHP"])
-        self.curso_combobox.grid(row=2, column=1, padx=5, pady=2)
+        ctk.CTkLabel(info_frame, text="Curso de Tecnologia:").grid(row=3, column=0, padx=5, pady=5, sticky="e")
+        self.curso_combobox = ctk.CTkComboBox(info_frame, values=["Python", "JavaScript", "Java", "PHP"])
+        self.curso_combobox.grid(row=3, column=1, padx=5, pady=2)
 
-        ctk.CTkLabel(frame, text="E-mail:").grid(row=3, column=0, padx=5, pady=2, sticky="e")
-        self.email_entry = ctk.CTkEntry(frame)
-        self.email_entry.grid(row=3, column=1, padx=5, pady=2)
+        ctk.CTkLabel(info_frame, text="E-mail:").grid(row=4, column=0, padx=5, pady=2, sticky="e")
+        self.email_entry = ctk.CTkEntry(info_frame)
+        self.email_entry.grid(row=4, column=1, padx=5, pady=2)
 
-        # Bloco 2: Botão
-        crud_frame = ctk.CTkFrame(self.app)
-        crud_frame.grid(row=0, column=1, padx=10, pady=5)
+        # Botão de adicionar aluno
+        crud_frame = ctk.CTkFrame(main_frame)
+        crud_frame.pack(anchor='n', side='left', padx=10, pady=10)
+        ctk.CTkButton(crud_frame, text="Adicionar", command=self.adicionar_aluno).pack(padx=5, pady=2)
 
-        ctk.CTkButton(crud_frame, text="Adicionar", command=self.adicionar_aluno).grid(row=0, column=0, padx=5, pady=2)
-
-        # Bloco 3: Lista de Alunos
-        self.lista_alunos = ctk.CTkTextbox(self.app, width=540, height=200) 
-        self.lista_alunos.grid(row=3, column=0, columnspan=2, padx=50, pady=5)
+        # Lista de Alunos
+        list_frame = ctk.CTkFrame(main_frame)
+        list_frame.pack(fill='both', expand=True, padx=10, pady=10)
+        self.lista_alunos = ctk.CTkTextbox(list_frame, width=540, height=200)
+        self.lista_alunos.pack(fill='both', expand=True)
 
     def adicionar_aluno(self):
         # Obtenha os valores dos campos da GUI
@@ -86,5 +98,5 @@ class Application():
         # Adicione os alunos à lista
         for aluno in alunos:
             # Concatene as informações do aluno para adicionar à lista
-            info_aluno = f"ID: {aluno['id']},  Nome: {aluno['nome']},  Curso: {aluno['nome_curso']},  Email: {aluno['email']}\n"
+            info_aluno = f"ID:  {aluno['id']}   Nome:   {aluno['nome']}  Curso:  {aluno['nome_curso']}   Email:  {aluno['email']}\n"
             self.lista_alunos.insert(tk.END, info_aluno)
