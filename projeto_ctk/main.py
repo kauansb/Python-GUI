730fe4c0a15c10bff3eb4b4bfb749e6865a1b6b9
Kauan <kauansbflu@gmail.com>
2024-03-01 11:02:47 -0300

    refatorando

--- projeto_ctk/main.py
+++ projeto_ctk/main.py
@@ -1,9 +1,7 @@
 import customtkinter as ctk
 from view.App import Application
-from view.AppCrud import Application2
 
 if __name__ == '__main__':
     root = ctk.CTk()
-    #app = Application(root)
-    app2 = Application2(root)
+    app = Application(root)
     root.mainloop()
